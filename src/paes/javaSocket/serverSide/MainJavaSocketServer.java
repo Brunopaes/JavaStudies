47542fb24bc462df92f12a232d0d5b85bdd31a12
Bruno Paes <brunopaes05@gmail.com>
2018-09-19 00:42:46 -0300

    Update

--- src/paes/javaSocket/serverSide/MainJavaSocketServer.java
+++ src/paes/javaSocket/serverSide/MainJavaSocketServer.java
@@ -8,20 +8,24 @@ public class MainJavaSocketServer {
 
         String corpo = "<h1>Oi! Boa Noite!</h1>";
 
-        String html = "HTTP/1.1 200\n" +
-                "Host: www.teste.com\n" +
+        String html = "HTTP/1.1 200 Ok\n" +
+                "Content-Type: text/html; charset = UTF-8\n" +
+                "Content-Length: " + corpo.length() + "\n\n" +
                 corpo;
 
+        System.out.println(html);
         try {
+            //Create a new service receiving in 80 door
             ServerSocket ss = new ServerSocket(80);
 
+            //Waiting for the requisitions
             Socket s = ss.accept();
             s.getOutputStream().write(html.getBytes());
 
             ss.close();
             s.close();
         }
-
+        
         catch (Exception e) { e.printStackTrace(); }
     }
 }
